c51ebdbfada245e6667286938405b62b3f56f8ef
Peachuvu <pfefferkornstefanie@gmail.com>
2023-06-08 15:34:09 +0200

    search Methode, aber geht noch nicht

--- Aufgabe3/gta_v3/public/javascripts/geotagging.js
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -5,13 +5,16 @@
 
 console.log("The geoTagging script is going to start...");
 
+var taglist = [];
+var taglist_json = JSON.stringify(document.getElementsByName('data-tags'));
+taglist.push(JSON.parse(taglist_json));
+var mapM = new MapManager("BvSIZ5qQ0kchef3XsC2M3bhrzefd11vE");
+
+
 function callback(position) {
     console.log("Loading callback(position)");
-    var testTags = [{latitude:49.01027, longitude:8.42080, name:"Pub"}];
-    var mapM = new MapManager("BvSIZ5qQ0kchef3XsC2M3bhrzefd11vE");
-    var taglist = [];
-    var taglist_json = JSON.stringify(document.getElementsByName('data-tags'));
-    taglist.push(JSON.parse(taglist_json));
+    // var testTags = [{latitude:49.01027, longitude:8.42080, name:"Pub"}];
+    // var mapM = new MapManager("BvSIZ5qQ0kchef3XsC2M3bhrzefd11vE");
 
 
     document.getElementById('latitude').value = position.latitude;
@@ -30,11 +33,9 @@ function updateLocation(){
 
 
     console.log("Loading updateLocation()");
-    var testTags = [{latitude:49.01027, longitude:8.42080, name:"Pub"}];
-    var mapM = new MapManager("BvSIZ5qQ0kchef3XsC2M3bhrzefd11vE");
-    var taglist = [];
-    var taglist_json = JSON.stringify(document.getElementsByName('data-tags'));
-    taglist.push(JSON.parse(taglist_json));
+    // var testTags = [{latitude:49.01027, longitude:8.42080, name:"Pub"}];
+    // var mapM = new MapManager("BvSIZ5qQ0kchef3XsC2M3bhrzefd11vE");
+
 
     var lat = document.getElementById('latitude').value;
     var long = document.getElementById('longitude').value;
@@ -47,6 +48,17 @@ function updateLocation(){
     var mapView = document.querySelector("#mapView");
     var mapurl = mapM.getMapUrl(lat, long, taglist, 16);
     mapView.setAttribute("src", mapurl);
+}
+
+
+function search() {
+    var searchterm = document.getElementById("searchterm");
+    for (var i = 0; i < taglist.length; i++) {
+        if (taglist[i].getAttribute("name") == searchterm) {
+            var mapurl = mapM.getMapUrl(taglist[i].getAttribute("latitude"), taglist[i].getAttribute("longitude"), searchterm, 16);
+            mapview.setAttribute("src", mapurl);
+        }
+    }
 }
 
 // Wait for the page to fully load its DOM content, then call updateLocation
