27ec16b81eebbbf63ae8ebe9db86236f91bda32f
Peachuvu <pfefferkornstefanie@gmail.com>
2023-06-24 23:56:52 +0200

    siehe comments

--- Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
+++ Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
@@ -6,11 +6,9 @@
 console.log("The geoTagging script is going to start...");
 
 var mapM = new MapManager("BvSIZ5qQ0kchef3XsC2M3bhrzefd11vE");
-const searchButton = document.getElementById('submitGeoTag');
 
 
 function parseTags() {
-    var taglist = [];
     const imageElement = document.getElementById('mapView');
     const taglist_json = imageElement.getAttribute('data-tags');
     return JSON.parse(taglist_json);
@@ -19,42 +17,117 @@ function parseTags() {
 function callback(position) {
     console.log("Loading callback(position)");
 
-
     document.getElementById('latitude').value = position.latitude;
     document.getElementById('longitude').value = position.longitude;
     document.getElementById('latitudesearch').value = position.latitude;
     document.getElementById('longitudesearch').value = position.longitude;
 
-    const taglist = parseTags();
-    const mapView = document.getElementById('mapView');
-    const mapurl = mapM.getMapUrl(position.latitude, position.longitude, taglist, 16);
-    mapView.setAttribute("src", mapurl);
+    updateURL(position.latitude, position.longitude);
 
 }
 
 
 function updateLocation(){
-
-
     console.log("Loading updateLocation()");
 
-
-    var lat = document.getElementById('latitude').value;
-    var long = document.getElementById('longitude').value;
+    var lat = document.getElementById("latitude").value;
+    var long = document.getElementById("longitude").value;
 
     if (lat === undefined || lat === "" || lat === "0") {
         console.log("Locating Device")
         LocationHelper.findLocation(callback);
+    } else {
+        updateURL(lat, long);
     }
 
+    return [lat, long];
+}
+
+
+//------------------ neu ------------------//
+
+//--- aktualisiere die map URL ---//
+
+function updateURL(latitude, longitude) {
+
     const taglist = parseTags();
-    const mapView = document.getElementById('mapView');
-    const mapurl = mapM.getMapUrl(lat, long, taglist, 16);
+    const mapView = document.getElementById("mapView");
+    const mapurl = mapM.getMapUrl(latitude, longitude, taglist, 16);
     mapView.setAttribute("src", mapurl);
 }
 
 
+
+//--- GeoTags rendern ---//
+
+function renderGeoTags(taglist) {
+    console.log("calling 'renderGeoTags'");
+    const geotaglist = document.getElementById("discoveryResults");
+
+    //fill the list//
+    geotaglist.replaceChildren(...taglist.map((geotag) => {
+        let listElement = document.createElement("li");
+        listElement.innerHTML = `${geotag.name} (${geotag.latitude}, ${geotag.longitude}) ${geotag.hashtag}`;
+        return listElement;
+    }));
+
+    const mapView = document.getElementById("mapView");
+    mapView.getAttribute('data-tags') = JSON.stringify(taglist);
+    
+    updateURL(...updateLocation());
+}
+
+
+//--- Tagging handling ---//
+//------ Seite muss neu geladen werden, sodass der GeoTag auf der Karte auftaucht ------//
+//------ in der Liste taucht er direkt auf ------//
+//------ neuer GeoTag taucht aber im VS Code Terminal direkt auf ------//
+//------ → das Aktualisieren der Karte allein funktioniert nicht richtig (?) ------//
+async function handleTaggingForm(submitEvent) {
+    // dafür da, dass nicht in '/tagging' geladen wird //
+    submitEvent.preventDefault();
+    
+    await fetch('/api/geotags', {
+        method : "post",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+
+        body : JSON.stringify({
+            latitude : document.getElementById('latitude').value,
+            longitude : document.getElementById('longitude').value,
+            name : document.getElementById('name').value,
+            hashtag : document.getElementById('hashtag').value
+        })
+    });
+
+    renderGeoTags(await (await fetch ("/api/geotags")).json());
+}
+
+
+//--- Discovery handling ---//
+//------ Suche wird überhaupt nicht ausgeführt ------//
+//------ also man kann auf den Button / Enter klicken, aber es passiert GAR nichts ------//
+async function handleDiscoveryForm(submitEvent) {
+    // dafür da, dass nicht in '/discovery' geladen wird //
+    submitEvent.preventDefault();
+    const [latitude, longitude] = updateLocation();
+    const query = document.getElementById("discovery-query").value;
+    let url = `/api/geotags?latitude=${latitude}&longitude=${longitude}`;
+    if (query != "") {
+        url += `&searchterm=${encodeURIComponent(query)}`;
+    }
+    const response = await fetch(url);
+    const responseBody = await response.json();
+    renderGeoTags(responseBody);
+}
+
+
+
+
 // Wait for the page to fully load its DOM content, then call updateLocation
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     updateLocation();
+    document.getElementById("tag-form").addEventListener("submit", handleTaggingForm);
+    document.getElementById("discoveryFilterForm").addEventListener("submit", handleDiscoveryForm);
 });
--- Aufgabe4/gta_v4_template/routes/index.js
+++ Aufgabe4/gta_v4_template/routes/index.js
@@ -99,8 +99,8 @@ router.post('/discovery', (req, res) => {
 router.get('/api/geotags', (req, res) => {
 
   let taglist = [];
-  const latitude = parseFloat(req.body.latitude);
-  const longitude = parseFloat(req.body.longitude);
+  const latitude = req.body.latitude;
+  const longitude = req.body.longitude;
   const searchterm = req.body.searchterm;
 
 //--- sind latitude und longitude gegeben, wird auf searchterm geprüft ---//
@@ -135,8 +135,8 @@ router.get('/api/geotags', (req, res) => {
 
   router.post('/api/geotags', (req, res) => {
 
-    const latitude = parseFloat(req.body.latitudesearch);
-    const longitude = parseFloat(req.body.longitudesearch);
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
     const name = req.body.name;
     const hashtag = req.body.hashtag;
 
@@ -188,12 +188,12 @@ router.get('/api/geotags/:id', (req, res) => {
 router.put('/api/geotags/:id', (req, res) =>  {
 
   const id = req.params.id;
-  const latitude = parseFloat(req.body.latitudesearch);
-  const longitude = parseFloat(req.body.longitudesearch);
+  const latitude = req.body.latitude;
+  const longitude = req.body.longitude;
   const name = req.body.name;
   const hashtag = req.body.hashtag;
 
-//--- finde den GeoTag mit angegebener ID und ersetze seine Attribute ---//
+//--- finde den GeoTag mit angegebener ID und ersetze ihn duch einen neuen ---//
   let geotag = database.replaceGeoTagById(id, new GeoTag(latitude, longitude, name, hashtag));
 
   res.json(geotag);
